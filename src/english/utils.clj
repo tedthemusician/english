b093de0e9bc1d6d448d77176f4a319ead33ae4a8
Ted Oliver <ted@teds-mbp.attlocal.net>
2021-03-31 22:19:24 -0400

    Add utility for loading contents of all files in dir

--- src/english/utils.clj
+++ src/english/utils.clj
@@ -1,7 +1,16 @@
 (ns english.utils
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io])
   (:gen-class))
 
 (defn map-vals
   "Map a function across the values of a hashmap"
   [f m]
   (reduce-kv (fn [acc k v] (update acc k f)) m m))
+
+(defn load-dir-files
+  [dir]
+  (let [sources (next (file-seq (io/file dir)))]
+    (map slurp sources)))
+
+(def load-dir-contents (comp (partial str/join " ") load-dir-files))
